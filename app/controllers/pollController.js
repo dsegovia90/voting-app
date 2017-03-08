46a1c0c36db849d0ed6e4fe9d3b94c928d79f3d9
Daniel Segovia <daniel@dabici.mx>
2017-03-07 22:40:33 -0600

    finished the basejump

--- app/controllers/pollController.js
+++ app/controllers/pollController.js
@@ -1,8 +1,8 @@
 var Polls = require('../models/polls.js')
 
-
 function PollHandler() {
 
+	//Creates poll from form and stores in db
 	this.createPoll = function(req, res){
 		var options = req.body.poll_options
 		options = options.split(' ')
@@ -22,6 +22,7 @@ function PollHandler() {
 		res.redirect('/')
 	}
 
+	//Checks db and sends all polls to views as 'polls'
 	this.showAll = function(req, res){
 		Polls.find({}, function(err, db_polls){
 			if(err) {throw err}
@@ -30,6 +31,7 @@ function PollHandler() {
 		})
 	}
 
+	//Checks db and sends one poll to views as 'poll'
 	this.showOne = function(req, res){
 		Polls.findOne({ _id: req.params._id }, function(err, db_poll){
 			if(err){throw err}
@@ -38,6 +40,7 @@ function PollHandler() {
 		})
 	}
 
+	//Checks db and sends all polls belonging to user to views as 'mypolls'
 	this.showMine = function(req, res) {
 		Polls.find({owner_id: req.user._id}, function(err, db_mypolls){
 			if(err){throw err}
@@ -46,27 +49,37 @@ function PollHandler() {
 		})
 	}
 
+	//Checks db and removes poll, user must be owner else it wont delete
 	this.deletePoll = function(req, res) {
 		Polls.remove({_id: req.params._id, owner_id: req.user._id}, function(err){
 			if(err) throw err
-			res.flash('danger', 'Poll deleted.')
-			res.redirect('/mypolls')
+			res.flash('danger', 'Poll deleted.').redirect('/mypolls')
 		})
 	}
 
+	//Votes on option and updates database count
 	this.vote = function(req, res) {
-		var selector = req.body.selector
-		var query = 'options_votes.' + selector
-		var option = 1 + parseInt(selector)
-		req.flash('info', 'You voted for option ' + option + '.') 
+		if(req.body.alternative === '' || req.body.alternative === undefined){
+			var selector = req.body.selector
+			var query = 'options_votes.' + selector
+			var option = 1 + parseInt(selector)
+			req.flash('info', 'You voted for option ' + option + '.') 
+			Polls.findOneAndUpdate({ _id: req.params._id }, { $inc: { [query]: 1 } }, function(err, result){
+				if(err) throw err
 
-
-		Polls.findOneAndUpdate({ _id: req.params._id}, { $inc: { [query]: 1 } }, function(err, result){
-			if(err) throw err
-
-			res.redirect('/poll/' + req.params._id)
-		})
-
+				res.redirect('/poll/' + req.params._id)
+			})
+		}else{
+			var newOption = req.body.alternative
+			Polls.findOne({ _id: req.params._id }, function(err, poll) {
+				if(err) throw err
+				poll.options.push(newOption)
+				poll.options_votes.push(1)
+				req.flash('info', 'You voted for option ' + newOption + '.') 
+				poll.save()
+				res.redirect('/poll/' + req.params._id)
+			})
+		}
 	}
 
 }
